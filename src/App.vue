8355ce298b3e58fe701faffa507fdec1d2aaeb75
Mark Yuan <mark@webasone.com>
2025-09-16 17:32:58 +0000

    update

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,17 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { jwtDecode } from 'jwt-decode'; // Named import
+const callback = (response) => {
+  // This callback will be triggered when the user selects or login to
+  // his Google account from the popup
+  console.log("Handle the response", response)
+  const decodedToken = jwtDecode(response.credential);
+  console.log(decodedToken);
+  // Access user data from the decoded token payload
+  const email = decodedToken.email;
+  const name = decodedToken.name;
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <GoogleLogin :callback="callback" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-
+// @ts-ignore
+import { createApp } from "vue";
+// @ts-ignore
+import App from "./App.vue";
+// @ts-ignore
+import vue3GoogleLogin from "vue3-google-login";
 
 let myWindow = window as any;
 
 const app = createApp(App);
-app.mount('#'+myWindow.asoneId)
+app.use(vue3GoogleLogin, {
+  clientId:
+    "764997431754-lqskpfikdo3vfh84abo01bn3dhgrbksb.apps.googleusercontent.com",
+});
+app.mount("#" + myWindow.asoneId);
