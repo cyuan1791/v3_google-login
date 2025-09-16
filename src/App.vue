d7649d049e00c1fbfca92adbb99f19b8cf1d40ef
Mark Yuan <mark@webasone.com>
2025-09-16 09:38:26 -0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,30 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
 <template>
-  <!-- use vue3 and bootstrap 4 to create a blogpost. The post data is read from a json file. The main page display the title, data, author, image, summary and a read more link to read the content
-  -->
-
-  <router-view v-slot="slotProps">
-    <transition name="fade" mode="out-in">
-      <component  :is="slotProps.Component"></component>
-    </transition>
-  </router-view>
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
 </template>
-<style lang="css">
 
-.fade-enter-from,
-.fade-button-leave-to {
-  opacity: 0;
-}   
-      
-.fade-enter-active {
-  transition: opacity 0.3s ease-out;
-}   
-      
-.fade-leave-active {
-  transition: opacity 0.3s ease-in;
-}   
-    
-.fade-enter-to,
-.fade-leave-from {
-  opacity: 1;
-}     
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,5 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
 
-let myWindow = window as any;
-
-const app = createApp(App);
-app.use(router);
-app.mount('#'+myWindow.asoneId)
+createApp(App).mount('#app')
